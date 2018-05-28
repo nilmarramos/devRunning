99a38f745b7f886173fd8f4b71184892ad5b3925
nilmar <nilmar@iMac-de-nilmar.local>
2018-05-28 15:42:17 -0300

    add login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  async componentDidMount() {
+    let token = localStorage.getItem('token')
+    if (!token) {
+	    const login = await axios.post('http://localhost:3001/users/login', {
+		    email: 'tuliofaria@devpleno.com',
+		    passwd: 'abc123'
+	    })
+	    const {token} = login.data
+	    localStorage.setItem('token', token)
+    }
+    const decoded = jwtDecode(token)
+    console.log(decoded)
+  }
+
+	render() {
     return (
       <div className="App">
         <header className="App-header">
