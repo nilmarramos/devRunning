8433a898f5254707f5a6c8180e2f7900e976f8de
nilmar <nilmar@iMac-de-nilmar.local>
2018-05-29 23:18:40 -0300

    add semanti-ui

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Route , BrowserRouter as Router} from 'react-router-dom'
 import store from './redux'
 import {Provider} from 'react-redux'
 
-import './App.css'
-import Header from "./Header";
 import Home from "./screens/Home";
 import Admin from "./screens/Admin";
 import Restrito from "./screens/Restrito";
@@ -18,16 +16,10 @@ class App extends Component {
       <Provider store={store}>
 	      <Router>
 		      <div className="App">
-
 			      <Route exact path="/" component={Home}/>
 			      <Route path="/admin" component={Admin}/>
 			      <Route path="/restrito" component={Restrito}/>
 			      <Route path="/login" component={Login}/>
-
-			      <Header/>
-			      <p className="App-intro">
-				      To get started, edit <code>src/App.js</code> and save to reload.
-			      </p>
 		      </div>
 	      </Router>
       </Provider>
--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,9 @@ import { connect } from 'react-redux'
 import ActionCreators from './redux/actionCreators'
 import {Link} from 'react-router-dom'
 
-import logo from './logo.svg'
-
 const Header = props => {
 	return (
 		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
 			<h1 className="App-title">Welcome to React</h1>
 			<p>
 				<Link to="/">Home</Link>
